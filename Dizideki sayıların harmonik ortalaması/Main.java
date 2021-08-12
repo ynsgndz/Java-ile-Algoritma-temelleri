5f65c18ae75606cac7b7fa4c05ab66159b456ba1
Yunus Gündüz <30951444+ynsgndz@users.noreply.github.com>
2021-08-12 13:55:06 +0300

    Add files via upload

---
+++ Dizideki sayıların harmonik ortalaması/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] sayilar={1,2,3,4,5,6};
+double toplam=0;
+      for (int i:sayilar){
+          toplam+=1/i;
+      }
+        System.out.println(sayilar.length/toplam);
+
+    }
+}
+    
